da800b433c7d216c6ffb081b73e8d4037238874e
louis <1221432@isep.ipp.pt>
2025-04-25 17:23:57 +0100

    update

--- src/main/java/org/example/AuditLogger.java
+++ src/main/java/org/example/AuditLogger.java
@@ -3,3 +3,9 @@ package org.example;
 public interface AuditLogger {
     void log(String message);
 }
+class ConsoleAuditLogger implements AuditLogger {
+    @Override
+    public void log(String message) {
+        System.out.println("Audit log: " + message);
+    }
+}
